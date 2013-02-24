2f1fd482e49f7d51bddd5f4070119d46cb22beba
Thomas Endres <Thomas-Endres@gmx.de>
2013-02-24 09:12:40 +0100

    Temporalily enabling the plug works now

--- src/main/java/com/tngtech/internal/plugclient/NetioPlugClient.java
+++ src/main/java/com/tngtech/internal/plugclient/NetioPlugClient.java
@@ -9,9 +9,14 @@ import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 public class NetioPlugClient implements PlugClient {
+    public static final int DEFAULT_TIMEOUT = 1000;
 
-    public static final int STATE_ON = 1;
-    public static final int STATE_OFF = 0;
+    public static final int STATUS_LOGIN = 100;
+    public static final int STATUS_QUIT = 110;
+    public static final int STATUS_OK = 250;
+
+    public static final int PLUG_ON = 1;
+    public static final int PLUG_OFF = 0;
 
     public static final DateTimeFormatter dateTimeFormatter = DateTimeFormat.forPattern("yyyy/MM/dd,HH:mm:ss");
 
@@ -38,31 +43,45 @@ public class NetioPlugClient implements PlugClient {
 
     public void login() {
         telnetClient.connect();
-        telnetClient.waitForMessage(new WaitForStatusCodePredicate(100), 1000);
-        telnetClient.send(getLoginMessage());
-        telnetClient.waitForMessage(new WaitForStatusCodePredicate(250), 1000);
+
+        waitForAcknowledge(STATUS_LOGIN);
+        sendAndWaitForAcknowledge(getLoginMessage(), STATUS_OK);
     }
 
     public void enablePlugPort() {
-        telnetClient.send(getPortMessage(STATE_ON));
+        sendAndWaitForAcknowledge(getPortEnableDisableMessage(PLUG_ON), STATUS_OK);
     }
 
     public void disablePlugPort() {
-        telnetClient.send(getPortMessage(STATE_OFF));
+        sendAndWaitForAcknowledge(getPortEnableDisableMessage(PLUG_OFF), STATUS_OK);
     }
 
     public void enablePlugTemporarily() {
-        telnetClient.send(getSystemTimeMessage());
-        String response = telnetClient.waitForMessage(new WaitForStatusCodePredicate(250), 1000);
+        String response = sendAndWaitForAcknowledge(getSystemTimeMessage(), STATUS_OK);
 
-        DateTime timeToStart = getSystemTime(response);
+        DateTime timeToStart = getTimeToStart(getSystemTime(response));
         DateTime timeToEnd = getTimeToEnd(timeToStart);
 
-        telnetClient.send(getEnablePortForPeriodOfTimeMessage(timeToStart, timeToEnd));
+        sendAndWaitForAcknowledge(getEnablePortForPeriodOfTimeMessage(timeToStart, timeToEnd), STATUS_OK);
+        sendAndWaitForAcknowledge(getTimerEnableMessage(), STATUS_OK);
     }
 
     public void disconnect() {
+        sendAndWaitForAcknowledge(getQuitMessage(), STATUS_QUIT);
         telnetClient.disconnect();
+    }
+
+    private String stripCommandCode(String dateText) {
+        return dateText.replaceAll("^[\\S]*", "").trim();
+    }
+
+    private String sendAndWaitForAcknowledge(String message, int statusCode) {
+        telnetClient.send(message);
+        return waitForAcknowledge(statusCode);
+    }
+
+    private String waitForAcknowledge(int statusCode) {
+        return telnetClient.waitForMessage(new WaitForStatusCodePredicate(statusCode), DEFAULT_TIMEOUT);
     }
 
     private DateTime getSystemTime(String timeMessageText) {
@@ -70,32 +89,46 @@ public class NetioPlugClient implements PlugClient {
         return dateTimeFormatter.parseDateTime(timeText);
     }
 
+    private DateTime getTimeToStart(DateTime systemTime) {
+        // The plug does not allow a start time that less than one minute away
+        return systemTime.plus(new Period(0, 1, 1, 0));
+    }
+
     protected DateTime getTimeToEnd(DateTime timeToStart) {
-        // TODO not accepted by the plug
         return timeToStart.plus(new Period(0, 0, 30, 0));
     }
 
-    private String stripCommandCode(String dateText) {
-        return dateText.replaceAll("^[\\S]*", "").trim();
-    }
-
     private String getLoginMessage() {
+        // login <name> <password>
+        // TODO clogin <name> <encrypted_password>
         return String.format("login %s %s", config.getAdminAccount(), config.getAdminPassword());
     }
 
-    private String getPortMessage(int newState) {
-        return String.format("port %d %d", config.getPlug().getPlugNumber(), newState);
+    private String getSystemTimeMessage() {
+        // system time <YYYY/MM/DD,HH:MM:SS>
+        return "system time";
     }
 
-    private String getSystemTimeMessage() {
-        return "system time";
+    private String getPortEnableDisableMessage(int newState) {
+        // port <output> [0 | 1 | manual | int]
+        return String.format("port %d %d", config.getPlug().getPlugNumber(), newState);
     }
 
     private String getEnablePortForPeriodOfTimeMessage(DateTime timeToStart, DateTime timeToEnd) {
         String timeToStartText = timeToStart.toString(dateTimeFormatter);
         String timeToEndText = timeToEnd.toString(dateTimeFormatter);
 
+        // port timer <output> <time_format> [ <mode: once | daily | weekly> <on-time> <off-time>] <week_schedule>
         return String.format("port timer %d dt once %s %s 1111111", config.getPlug().getPlugNumber(), timeToStartText,
                 timeToEndText);
+    }
+
+    private String getTimerEnableMessage() {
+        // port setup <output> [ <output_name> <mod: manual|timer> <interrupt_delay> <PON_status> ]
+        return String.format("port setup %d %s timer 0 0", config.getPlug().getPlugNumber(), config.getPlug().name());
+    }
+
+    public String getQuitMessage() {
+        return "quit";
     }
 }
--- src/test/java/com/tngtech/internal/plugclient/NetioPlugClientTest.java
+++ src/test/java/com/tngtech/internal/plugclient/NetioPlugClientTest.java
@@ -18,7 +18,6 @@ import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.inOrder;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SuppressWarnings("unchecked")
@@ -55,45 +54,70 @@ public class NetioPlugClientTest {
         inOrder.verify(telnetClient).waitForMessage(captorForLoginMessagePredicate.capture(), eq(1000));
 
         assertThatPredicateReturnsTrueForCode(captorForConnectMessagePredicate.getValue(), 100);
-        assertThatPredicateReturnsTrueForCode(captorForLoginMessagePredicate.getValue(), 250);
+        assertThatPredicateReturnsTrueForCode(captorForLoginMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
     }
 
     @Test
     public void testEnablePlugPort() {
         client.enablePlugPort();
 
+        ArgumentCaptor<Predicate> captorForEnablePlugMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
+
         InOrder inOrder = inOrder(telnetClient);
         inOrder.verify(telnetClient).send("port 2 1");
-    }
+        inOrder.verify(telnetClient).waitForMessage(captorForEnablePlugMessagePredicate.capture(), eq(1000));
 
-    @Test
-    public void testEnablePlugTemporarily() {
-        when(telnetClient.waitForMessage(any(Predicate.class), anyInt())).thenReturn("250 2012/12/31,23:59:45");
-
-        client.enablePlugTemporarily();
-
-        ArgumentCaptor<Predicate> captorForSystemTimeMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
-
-        InOrder inOrder = inOrder(telnetClient);
-        inOrder.verify(telnetClient).send("system time");
-        inOrder.verify(telnetClient).waitForMessage(captorForSystemTimeMessagePredicate.capture(), eq(1000));
-        inOrder.verify(telnetClient).send("port timer 2 dt once 2012/12/31,23:59:45 2013/01/01,00:00:15 1111111");
-
-        assertThatPredicateReturnsTrueForCode(captorForSystemTimeMessagePredicate.getValue(), 250);
+        assertThatPredicateReturnsTrueForCode(captorForEnablePlugMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
     }
 
     @Test
     public void testDisablePlugPort() {
         client.disablePlugPort();
 
+        ArgumentCaptor<Predicate> captorForDisableMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
+
         InOrder inOrder = inOrder(telnetClient);
         inOrder.verify(telnetClient).send("port 2 0");
+        inOrder.verify(telnetClient).waitForMessage(captorForDisableMessagePredicate.capture(), eq(1000));
+
+        assertThatPredicateReturnsTrueForCode(captorForDisableMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
+    }
+
+    @Test
+    public void testEnablePlugTemporarily() {
+        when(telnetClient.waitForMessage(any(Predicate.class), anyInt())).thenReturn("250 2012/12/31,23:58:44");
+
+        client.enablePlugTemporarily();
+
+        ArgumentCaptor<Predicate> captorForSystemTimeMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
+        ArgumentCaptor<Predicate> captorForSetTimerMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
+        ArgumentCaptor<Predicate> captorForSetupPortMessagePredicate = ArgumentCaptor.forClass(Predicate.class);
+
+        InOrder inOrder = inOrder(telnetClient);
+        inOrder.verify(telnetClient).send("system time");
+        inOrder.verify(telnetClient).waitForMessage(captorForSystemTimeMessagePredicate.capture(), eq(1000));
+        inOrder.verify(telnetClient).send("port timer 2 dt once 2012/12/31,23:59:45 2013/01/01,00:00:15 1111111");
+        inOrder.verify(telnetClient).waitForMessage(captorForSetTimerMessagePredicate.capture(), eq(1000));
+        inOrder.verify(telnetClient).send("port setup 2 PLUG2 timer 0 0");
+        inOrder.verify(telnetClient).waitForMessage(captorForSetupPortMessagePredicate.capture(), eq(1000));
+
+        assertThatPredicateReturnsTrueForCode(captorForSystemTimeMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
+        assertThatPredicateReturnsTrueForCode(captorForSetTimerMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
+        assertThatPredicateReturnsTrueForCode(captorForSetupPortMessagePredicate.getValue(), NetioPlugClient.STATUS_OK);
     }
 
     @Test
     public void testDisconnect() {
         client.disconnect();
-        verify(telnetClient).disconnect();
+
+        ArgumentCaptor<Predicate> captorForQuitMessage = ArgumentCaptor.forClass(Predicate.class);
+
+        InOrder inOrder = inOrder(telnetClient);
+        inOrder.verify(telnetClient).send("quit");
+        inOrder.verify(telnetClient).waitForMessage(captorForQuitMessage.capture(), eq(1000));
+        inOrder.verify(telnetClient).disconnect();
+
+        assertThatPredicateReturnsTrueForCode(captorForQuitMessage.getValue(), NetioPlugClient.STATUS_QUIT);
     }
 
     private void assertThatPredicateReturnsTrueForCode(Predicate<String> predicate, int code) {
