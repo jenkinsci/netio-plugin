184e0ee55f485dffb35558bed95eded35c073232
Thomas Endres <Thomas-Endres@gmx.de>
2013-02-25 22:06:59 +0100

    Finished plug sender tests

--- src/test/java/com/tngtech/internal/plug/PlugSenderTest.java
+++ src/test/java/com/tngtech/internal/plug/PlugSenderTest.java
@@ -1,41 +1,69 @@
 package com.tngtech.internal.plug;
 
+import com.tngtech.internal.context.Context;
+import com.tngtech.internal.plugclient.PlugClient;
+import com.tngtech.internal.plugclient.PlugClientCreator;
 import hudson.model.BuildListener;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.io.PrintStream;
 
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.verifyStatic;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Context.class)
 public class PlugSenderTest {
-
-    private PlugSender plugSender;
-
     @Mock
     private BuildListener buildListener;
 
     @Mock
     private PrintStream logger;
 
+    @Mock
+    private PlugClientCreator plugClientCreator;
+
+    @Mock
+    private PlugClient plugClient;
+
     private PlugConfig plugConfig;
+
+    private PlugSender plugSender;
 
     @Before
     public void setUp() {
-        //plugConfig = new PlugConfig("hostName", 80, "adminAccount", "adminPassword", "PLUG1");
-        //plugSender = new PlugSender();
+        PowerMockito.mockStatic(Context.class);
+        when(Context.getBean(PlugClientCreator.class)).thenReturn(plugClientCreator);
+        when(plugClientCreator.withPlugConfig(any(PlugConfig.class))).thenReturn(plugClientCreator);
+        when(plugClientCreator.createClient()).thenReturn(plugClient);
 
-        //when(buildListener.getLogger()).thenReturn(logger);
+        when(buildListener.getLogger()).thenReturn(logger);
+
+        plugConfig = new PlugConfig("hostName", 80, "adminAccount", "adminPassword", "PLUG1");
+        plugSender = new PlugSender();
     }
 
     @Test
     public void testSend() {
-        //plugSender.send(buildListener, plugConfig);
-        //verify(logger).println("Using connection to adminAccount:adminPassword@hostName:80 for plug number 1");
+        plugSender.send(buildListener, plugConfig);
+        verify(logger).println("Using connection to adminAccount:adminPassword@hostName:80 for plug number 1");
+
+        verifyStatic();
+        Context.getBean(PlugClientCreator.class);
+
+        verify(plugClientCreator).withPlugConfig(plugConfig);
+        verify(plugClientCreator).createClient();
+
+        verify(plugClient).login();
+        verify(plugClient).enablePlugPortTemporarily();
+        verify(plugClient).disconnect();
     }
 }
